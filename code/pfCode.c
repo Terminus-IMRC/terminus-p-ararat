53c21a3714bf42a459e586737d71fd8823321aef
Terminus <i.can.speak.c.and.basic@gmail.com>
2013-06-10 10:46:50 +0900

    The file pointer pointed an illegal value.

--- code/pfCode.c
+++ code/pfCode.c
@@ -5,8 +5,8 @@ void pfCode(usetype code[X][X]){
 	int i, j;
 	for(i=0; i<X; i++){
 		for(j=0; j<X; j++)
-			fprintf(FP, "%2d ", code[j][i]);
-		fputc('\n', PF);
+			fprintf(myfp, "%2d ", code[j][i]);
+		fputc('\n', myfp);
 	}
 	#endif
 	return;
