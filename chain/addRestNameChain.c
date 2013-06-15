1212e031d9563087ffe9c8be6d385337c0dc5374
Terminus <i.can.speak.c.and.basic@gmail.com>
2013-06-15 20:39:36 +0900

    Sorry. I forgot to add this source.

---
+++ chain/addRestNameChain.c
@@ -0,0 +1,31 @@
+#include "chain.h"
+
+void addRestNameChain(int l, int m, _Bool isname1){
+	int i, ti;	/*love and true love*/
+
+	prepcode[l][m]=AutoFilled;
+	tate[l]++;
+	yoko[m]++;
+	chain[chaincont].toafill[chain[chaincont].toafillcont].x=l;
+	chain[chaincont].toafill[chain[chaincont].toafillcont].y=m;
+	for(i=ti=0; i<X; i++){
+		if(i==m)
+			continue;
+		if(isname1)
+			chain[chaincont].toafillroad[chain[chaincont].	\
+				toafillcont][ti].x=X-1-i;
+		else
+			chain[chaincont].toafillroad[chain[chaincont].	\
+				toafillcont][ti].x=i;
+		chain[chaincont].toafillroad[chain[chaincont].	\
+			toafillcont][ti].y=i;
+
+		ti++;
+	}
+	chain[chaincont].toafillcont++;
+
+	addChainOrder(prepcode, l, m, False);
+
+	return;
+}
+
