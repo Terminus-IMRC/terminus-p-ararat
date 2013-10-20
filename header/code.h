a5e4e7ee74c203b78c779ee9b769ee93ee91f81d
Terminus <i.can.speak.c.and.basic@gmail.com>
2013-10-20 12:40:46 +0900

    Created tcode-like-array saving linear-list structre system.

--- header/code.h
+++ header/code.h
@@ -10,8 +10,20 @@
 	/*20130113*/
 	extern signed short int** tcode;
 	extern signed short int* tcode_as_1dim;
+	struct tcode_linear_list{
+		signed short int tcode_as_1dim[Ceilings];
+		struct tcode_linear_list* next;
+	};
 	extern unsigned char* dned;
 	extern signed short int *sum_tate, *sum_yoko, *sum_name;
+
+	struct tcode_linear_list* tcode_linear_list_get_new_entry();
+	void tcode_linear_list_initialize(struct tcode_linear_list* s);
+	void tcode_linear_list_tcode_mv(signed short int dest[Ceilings], signed short int* source);
+	void tcode_linear_list_subst(struct tcode_linear_list** dest, signed short int* localtcode_as_1dim);
+	void tcode_linear_list_output(FILE* fp, struct tcode_linear_list* s);
+	void tcode_linear_list_output_from_orig(FILE* fp, struct tcode_linear_list* orig);
+	void tcode_linear_list_free_from_orig(struct tcode_linear_list* orig);
 
 	/*20130202*/
 	/*extern int total;*/
---
+++ tcode/tcode_linear_list_utils.c
@@ -0,0 +1,72 @@
+#include <assert.h>
+#include <stdlib.h>
+#include "code.h"
+
+void tcode_linear_list_initialize(struct tcode_linear_list* s)
+{
+	s->next=NULL;
+	return;
+}
+
+struct tcode_linear_list* tcode_linear_list_get_new_entry()
+{
+	struct tcode_linear_list* stl;
+
+	stl=(struct tcode_linear_list*)malloc(sizeof(struct tcode_linear_list));
+	assert(stl);
+	tcode_linear_list_initialize(stl);
+
+	return stl;
+}
+
+void tcode_linear_list_subst(struct tcode_linear_list** dest, signed short int* localtcode_as_1dim)
+{
+	tcode_linear_list_tcode_mv((*dest)->tcode_as_1dim, localtcode_as_1dim);
+	(*dest)->next=tcode_linear_list_get_new_entry();
+	*dest=(*dest)->next;
+	return;
+}
+
+void tcode_linear_list_tcode_mv(signed short int dest[Ceilings], signed short int* source)
+{
+	int i;
+
+	for(i=0; i<Ceilings; i++)
+		dest[i]=source[i];
+
+	return;
+}
+
+void tcode_linear_list_output(FILE* fp, struct tcode_linear_list* s)
+{
+	int i;
+
+	for(i=0; i<Ceilings; i++)
+		fprintf(fp, "%d ", s->tcode_as_1dim[i]);
+	fputc('\n', fp);
+
+	return;
+}
+
+void tcode_linear_list_output_from_orig(FILE* fp, struct tcode_linear_list* orig)
+{
+	while(orig->next){
+		tcode_linear_list_output(fp, orig);
+		orig=orig->next;
+	}
+
+	return;
+}
+
+void tcode_linear_list_free_from_orig(struct tcode_linear_list* orig)
+{
+	struct tcode_linear_list* next;
+
+	while(orig){
+		next=orig->next;
+		free(orig);
+		orig=next;
+	}
+
+	return;
+}
