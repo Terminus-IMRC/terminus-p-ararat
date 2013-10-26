cc8fb1bfe5db53cc6f3c34a74bf4948ed0532d7d
Terminus <i.can.speak.c.and.basic@gmail.com>
2013-10-26 10:50:13 +0000

    Changed pfTcode to output only to stdout.
    Because myfp will be unsupported.

--- tcode/pfTcode.c
+++ tcode/pfTcode.c
@@ -1,20 +1,14 @@
 #include "def.h"
 
-#ifdef PF
-#define FP myfp
-#else
-#define FP stdout
-#endif
-
 #if 0
 /*When tcode is 1-dimentional array*/
 void pfTcode(signed short int* code)
 {
 	int i;
 	for(i=0; i<X*X; i++){
-		fprintf(FP, "%2d ", code[i]);
+		printf("%2d ", code[i]);
 		if(i%X==X-1)
-			fputc('\n', FP);
+			putchar('\n');
 	}
 	return;
 }
@@ -24,8 +18,8 @@ void pfTcode(signed short int** code)
 	int i, j;
 	for(i=0; i<X; i++){
 		for(j=0; j<X; j++)
-			fprintf(FP, "%2d ", code[j][i]);
-		fputc('\n', FP);
+			printf("%2d ", code[j][i]);
+		putchar('\n');
 	}
 	return;
 }
